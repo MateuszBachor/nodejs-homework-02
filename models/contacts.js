70e4444ac1881d7f3820f1e597ff1a1f6b58f8e7
MateuszBachor <mateuszbachor3@gmail.com>
2022-10-28 03:17:58 +0200

    Start

--- models/contacts.js
+++ models/contacts.js
@@ -1,16 +1,69 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./models/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.error("Load error", error.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const jsonContacts = await JSON.parse(contacts);
+    return jsonContacts.filter((contact) => contact.id === contactId.toString());
+  } catch (error) {
+    console.error("Get contact error", error.message);
+  }
+};
 
-const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await fs.readFile(contactsPath);    
+    const jsonContacts = await JSON.parse(contacts);
+    const contact = jsonContacts.filter((contact) => contact.id === contactId.toString());
+    const removedContact = await jsonContacts.filter((contact) => contact.id !== contactId.toString());
+    await fs.writeFile(contactsPath, JSON.stringify(removedContact));
+    return contact;
+  } catch (error) {
+    console.error( error.message);
+  }
+};
 
-module.exports = {
+const addContact = async (body) => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const jsonContacts = await JSON.parse(contacts);
+    const newContact = {...body, id:(jsonContacts.length +1).toString()};
+    const newContacts = [...jsonContacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    return newContact;
+  } catch (error) {
+    console.error( error.message);
+  }
+}
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    const jsonContacts = await JSON.parse(contacts);
+    let newArray = jsonContacts.filter((contact) => contact.id !== contactId.toString());
+    const newContact = { ...body, id: contactId };
+    newArray = [...newArray, newContact]
+    await fs.writeFile(contactsPath, JSON.stringify(newArray));
+    return newContact;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+module.exports = { 
   listContacts,
   getContactById,
   removeContact,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,38 @@
 const express = require('express')
-
+const contacts = require('./../../models/contacts');
 const router = express.Router()
 
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactFunction = await contacts.listContacts()
+  res.status(200).json(contactFunction)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const contactFunction = await contacts.getContactById(contactId)
+ res.status(200).json(contactFunction)
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const newContact = req.body
+  const contactFunction = await contacts.addContact(newContact)
+  res.status(201).json(contactFunction)
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params
+  const contactFunction = await contacts.removeContact(contactId)
+  res.status(200).json(contactFunction)
+
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const newContact = req.body
+  const { contactId } = req.params
+  const contactFunction = await contacts.updateContact(contactId, newContact)
+  res.status(200).json(contactFunction)
+
 })
 
 module.exports = router
